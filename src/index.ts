3d85fad85fbc80c6fb3d4d98b775de017a75af64
Pratik Rajbhandari <r.pratik013@gmail.com>
2023-12-28 17:13:31 +0545

    Setup express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('hello world')
+import express from "express";
+
+import config from "./config";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello world",
+  });
+});
+
+console.log(`Server listening on port: ${config.serverPort}`);
+
+app.listen(config.serverPort);
