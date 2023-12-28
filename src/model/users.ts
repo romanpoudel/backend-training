54de68a7975d2d7dc6c73fdcdc3a30bca9ea2385
Pratik Rajbhandari <r.pratik013@gmail.com>
2023-12-28 22:19:54 +0545

    Update users

--- src/model/users.ts
+++ src/model/users.ts
@@ -1,20 +1,26 @@
-const users = [
+export const users = [
   {
     id: 1,
     name: "User 1",
+    email: "user1@test.com",
+    password: "$2b$10$W./GL4g9fKuIyLYzH6BsQe7LzvX2l.uEcintM5LyhQN4miHPfHlwa",
   },
   {
     id: 2,
     name: "User 2",
+    email: "user2@test.com",
+    password: "$2b$10$sPJYyf75p6V/GPfxBhmNL.vqIlY.o65IYk4CWfpXQWnoW8AIffCEe",
   },
   {
     id: 3,
     name: "User 3",
+    email: "user3@test.com",
+    password: "$2b$10$tW.NR6oPKAIa6BPRX5fs0eM7Py4rxUbVOysRafdxWp4MULE9wvjVW",
   },
 ];
 
 export const getUsers = (params: any) => {
-  let data = users;
+  let data = users.map((user) => ({ id: user.id, name: user.name }));
 
   if (params.name) {
     data = data.filter(({ name }) => name === params.name);
@@ -24,7 +30,8 @@ export const getUsers = (params: any) => {
 };
 
 export const getUserById = (id: number) => {
-  const user = users.find(({ id: userId }) => userId === id);
-
+  const user = users
+    .map((user) => ({ id: user.id, name: user.name }))
+    .find(({ id: userId }) => userId === id);
   return user;
 };
--- src/service/auth.ts
+++ src/service/auth.ts
@@ -1,8 +1,9 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-import { ISignUp } from "../interface/auth";
 import config from "../config";
+import { users } from "../model/users";
+import { ISignUp } from "../interface/auth";
 import { ACCESS_TOKEN_EXPIRY, REFRESH_TOKEN_EXPIRY } from "../constant/jwt";
 
 const SALT_ROUNDS = 10;
@@ -17,16 +18,13 @@ export const signup = async (body: ISignUp) => {
 };
 
 export const login = async (body: ISignUp) => {
-  const passwordMatch = await bcrypt.compare(
-    body.password,
-    "$2b$10$5vLyEtfGWAvUZDmRjgaJkuQOHk1ON4H.plU7RjvhRTbBp8xOBi9km"
-  );
+  const user = users.find(({ email }) => email === body.email)!;
+
+  const passwordMatch = await bcrypt.compare(body.password, user.password);
 
   if (!passwordMatch) {
     throw new Error("Unauthorized");
   }
-
-  const user = { id: 1, name: "User 1" };
 
   const accessToken = jwt.sign(user, config.jwt.accessTokenSecret!, {
     expiresIn: ACCESS_TOKEN_EXPIRY,
