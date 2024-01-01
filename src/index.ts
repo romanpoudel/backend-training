9531b1db8083f7a044dcf97d0e5b85f886e9fdcd
Pratik Rajbhandari <r.pratik013@gmail.com>
2024-01-01 20:49:32 +0545

    Add logger middleware

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ import express from "express";
 
 import config from "./config";
 import routes from "./routes";
+import { logger } from "./middleware/logger";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(logger);
 
 app.use(routes);
 
